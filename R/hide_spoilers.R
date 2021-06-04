db6a9a26af2cfa6d3fe164dc12939537dfc96a43
etiennebacher <etienne.bacher@protonmail.com>
2021-06-04 22:22:01 +0200

    Polish package: * reunite functions in a single file * finish doc * R CMD check 0 0 0

--- R/hide_spoilers.R
+++
@@ -1,7 +0,0 @@
-hide_spoilers <- function(max = 10, partial = 4) {
-  htmltools::tags$script(
-    paste0(
-      "spoilerAlert('spoiler, .spoiler', {max: ", max, ", partial: ", partial, "});"
-    )
-  )
-}
--- R/spoiler.R
+++ R/spoiler.R
@@ -1,3 +1,49 @@
+#' Use Spoiler-alert
+#'
+#' Load Spoiler-alert dependencies, mark some elements as spoilers, and define
+#' the blurring intensity for these elements.
+#'
+#' @export
+#' @rdname spoiler
+#'
+#' @examples
+#' if (interactive()) {
+#'   library(shiny)
+#'   library(spoiler)
+#'
+#'   # works better in browser
+#'
+#'   ui <- fluidPage(
+#'     # Load dependencies
+#'     use_spoiler(),
+#'     column(
+#'       3,
+#'       p("this text can be seen"),
+#'       spoiler(p("this one can't be seen")) # This text will be blurred
+#'     ),
+#'     hide_spoilers()
+#'   )
+#'
+#'   server <- function(input, output, session) {}
+#'
+#'   shinyApp(ui, server)
+#' }
+
+use_spoiler <- function() {
+
+  shiny::singleton(
+    shiny::tags$head(
+      shiny::tags$script(
+        src = "spoiler-assets/spoiler.min.js"
+      )
+    )
+  )
+}
+
+#' @param ui_element Element to blur
+#'
+#' @rdname spoiler
+
 spoiler <- function(ui_element) {
 
   htmltools::tagAppendAttributes(
@@ -6,3 +52,18 @@ spoiler <- function(ui_element) {
   )
 
 }
+
+#' @param max Blurring intensity of the element by default
+#'
+#' @param partial Blurring intensity when hovering the element
+#'
+#' @rdname spoiler
+
+hide_spoilers <- function(max = 10, partial = 4) {
+  htmltools::tags$script(
+    paste0(
+      "spoilerAlert('spoiler, .spoiler', {max: ", max, ", partial: ", partial, "});"
+    )
+  )
+}
+
